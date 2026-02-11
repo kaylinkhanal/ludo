7b5e0bfab1663bc5a05db3527a400fe89dcdce00
DikpalChaudhary <you@example.com>
2026-02-11 16:24:23 +0545

    level4

---
+++ numbers/level4.js
@@ -0,0 +1,13 @@
+// Q1. Calculate 5 to the power of 3 (5 * 5 * 5)
+const power = 5
+const base =3
+const result = 3**5
+console.log(result)
+//Q2. Find the Square Root of a number
+const squareroot = Math.sqrt(64)
+console.log(squareroot)
+
+// Q3. Get the integer part of a number by removing all decimals (without rounding)
+const num = 235.142
+const integer=Math.trunc(num)
+console.log(integer)
