ab584a98bb4c8a4c1271792677fc97f20720bec6
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-11 08:28:33 +0545

    numbers

--- numbers/level1.js
+++ numbers/level1.js
@@ -1,6 +1,12 @@
 //Q1 create two varaibles a and b, assign them values, and add their sum to c
+let a = 40
+let b = 60
+let c = a + b
+
+console.log(c)
 
 //Q2. Create a variable called d and assign it the value of a multiplied by b
+
 
 //Q3. convert the type of age to number
 const age = '50'
