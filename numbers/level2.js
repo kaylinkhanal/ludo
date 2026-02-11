5a63c2bd3950cb0e106485702ddcc81b7dff6f5a
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-11 08:58:17 +0545

    numbers

--- numbers/level2.js
+++ numbers/level2.js
@@ -3,11 +3,18 @@ const number = 0.3445223
 
 // Q1. output should be 0.34
 
+console.log(number.toFixed(2))
+
+
 
 
 const age = -30
 
 // Q2 age cannot be negative, output should be 30
+const output =Math.abs(age)
+console.log(output)
+
+
 
 
 
