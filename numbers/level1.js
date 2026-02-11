eaa4b61d34cd6f8113fd3e7bea6128abca9ed0f9
kaylinkhanal10 <kyalinnop@gmail.com>
2026-02-11 08:19:40 +0545

    add numbers level1

---
+++ numbers/level1.js
@@ -0,0 +1,8 @@
+//Q1 create two varaibles a and b, assign them values, and add their sum to c
+
+//Q2. Create a variable called d and assign it the value of a multiplied by b
+
+//Q3. convert the type of age to number
+const age = '50'
+
+//Q4 caclualte the remainder of 10 divided by 3 and assign it to a variable called remainder
