3db99cc8e6cf154bf305560c9fcee4a97d5f4335
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-11 08:38:18 +0545

    Solved the level2 questions

--- numbers/level2.js
+++ numbers/level2.js
@@ -2,7 +2,8 @@ const number = 0.3445223
 
 
 // Q1. output should be 0.34
-
+let fixedNumber=number.toFixed(2)
+console.log("Q1 Answer:", fixedNumber);
 
 
 const age = -30
@@ -10,4 +11,5 @@ const age = -30
 // Q2 age cannot be negative, output should be 30
 
 
-
+ let absAge=Math.abs(age)
+console.log("Q2 Answer:", absAge);
