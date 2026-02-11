06cdf311373353b5aedc79ec04a0d68905d7114f
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-11 09:06:57 +0545

    numbers

---
+++ numbers/level3.js
@@ -0,0 +1,27 @@
+/*Q1: The Discount Checker Create a variable price = 100. 
+If the price is greater than 50, subtract 10 from it. Otherwise, keep it the same. Output the final price.*/
+
+
+
+/*Q2: Secret Password Create a variable userInput = "1234". 
+Write an if statement: if the input is "1234", print "Access Granted." Otherwise, print "Access Denied."*/
+
+
+
+/*Q3: Temperature Check Create a variable temp = 15. 
+If the temp is below 20, print "Wear a jacket." If it's 20 or above, print "T-shirt weather."*/
+
+
+
+/*Q4: The Name Fixer Create a variable username = "jOhN". 
+Write code to ensure the first letter is capitalized and the rest are lowercase. (Output should be "John").*/
+
+
+
+/*Q5: The Range Checker Create a variable score = 85.
+ Write an if statement that prints "Pass" if the score is 50 or higher, and "Fail" if it is lower.*/
+
+
+
+/*Q6: Array Basics Create an array of three fruits.
+ Add a fourth fruit to the end of the list and then remove the first fruit.*/
