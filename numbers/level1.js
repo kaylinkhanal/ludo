8aaf1be0951e97705c7379007914958ba65d0172
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-11 08:27:57 +0545

    Solved the level1 questions

--- numbers/level1.js
+++ numbers/level1.js
@@ -1,8 +1,18 @@
 //Q1 create two varaibles a and b, assign them values, and add their sum to c
-
+let a=2
+let b=4
+let c=a+b;
+console.log("Sum is :", c);
 //Q2. Create a variable called d and assign it the value of a multiplied by b
-
+let d=a*b;
+console.log("D:", d);
 //Q3. convert the type of age to number
 const age = '50'
+let ageInNumber=Number(age);
+console.log(typeof(ageInNumber));
+
 
 //Q4 caclualte the remainder of 10 divided by 3 and assign it to a variable called remainder
+
+let reminder=10%3;
+console.log("reminder is:", reminder);
