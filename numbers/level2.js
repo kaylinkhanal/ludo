2e82c19e41ea7d06df0d4cd77f8d7f76979d5551
Satish Shrestha <satish020820@gmail.com>
2026-02-11 08:42:45 +0545

    ans for 2nd

--- numbers/level2.js
+++ numbers/level2.js
@@ -1,11 +1,14 @@
 const number = 0.3445223
-
+console.log(number.toFixed(2))
 
 // Q1. output should be 0.34
 
 
 
 const age = -30
+if (age < 0) {
+    console.log(30)
+}
 
 // Q2 age cannot be negative, output should be 30
 
