40833d77385f085c1b0c48ada8b58e0f6c22fbd7
Satish Shrestha <satish020820@gmail.com>
2026-02-11 08:27:22 +0545

    ans for level1 js

--- numbers/level1.js
+++ numbers/level1.js
@@ -1,8 +1,18 @@
 //Q1 create two varaibles a and b, assign them values, and add their sum to c
+let a = 15
+let b = 25
+let c = a + b
+console.log(c)
 
 //Q2. Create a variable called d and assign it the value of a multiplied by b
+let d= a * b
+console.log(d)
 
 //Q3. convert the type of age to number
 const age = '50'
+const ageNumber = Number(age)
+console.log(ageNumber)
 
 //Q4 caclualte the remainder of 10 divided by 3 and assign it to a variable called remainder
+const e = 10 % 3
+console.log(e)
