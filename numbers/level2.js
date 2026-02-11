b828c30a69f3209cbda3eb1b90dc883058cff1f7
DikpalChaudhary <you@example.com>
2026-02-11 08:57:59 +0545

    add level2

--- numbers/level2.js
+++ numbers/level2.js
@@ -1,13 +1,14 @@
 const number = 0.3445223
-let output=number *
-console.log(output)
+const result =number.toFixed(2)
+console.log(result)
 
 // Q1. output should be 0.34
 
 
 
 const age = -30
-
+const  output =Math.abs(age)
+console.log(output)
 // Q2 age cannot be negative, output should be 30
 
 
