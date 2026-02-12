102ec0632c922f8d3b466ea6247cb162a398a46e
Satish Shrestha <satish020820@gmail.com>
2026-02-12 08:33:33 +0545

    level 1 string

---
+++ strings/level1.js
@@ -0,0 +1,17 @@
+const name = 'Ram Thapa'
+
+
+// Q1. output should be 'Hello Ram Thapa, welcome to JavaScript!'
+const hello = `Hello ${name}, welcome to JavaScript!`
+console.log(hello)   
+
+
+//Q2. Output should be 9
+const nameLength = name.length
+console.log(nameLength)
+
+
+// Q3. output should be Ra
+
+const firstTwoLetters = name.slice(0, 2)
+console.log(firstTwoLetters)
