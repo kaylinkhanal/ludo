3eb137cc2e795bb485dd374cfc0aecf73eb24698
Santosh Parajuli <santosh.08045@gmail.com>
2026-02-12 09:11:01 +0545

    solved boolean level 1 questions

--- boolean/level1.js
+++ boolean/level1.js
@@ -2,8 +2,9 @@ let a =10
 let b = 20
 
 // Q1. check if a is greater than b
-
+console.log(a==b);
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
-
+console.log((a*10)>=b);
 // Q3. check if b is divisible by a
+console.log((b%a)==0);
