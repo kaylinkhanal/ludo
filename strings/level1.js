9cddd458fbbf55212ac431f2f467c038433e573f
DikpalChaudhary <you@example.com>
2026-02-12 08:36:46 +0545

    level1

--- strings/level1.js
+++ strings/level1.js
@@ -1,11 +1,13 @@
 const name = 'Ram Thapa'
-
-
+const nameR='hello'
+const fullname= nameR + " "+ name 
+console.log(fullname +"welcome to javascript")
 // Q1. output should be 'Hello Ram Thapa, welcome to JavaScript!'
 
-
+console.log(name.length)
 //Q2. Output should be 9
-
+const result =name.substring(0,2)
+console.log(result)
 
 // Q3. output should be Ra
 
