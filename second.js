8351356fa0e6955f6bfb116da5dbf1f4989055a1
PyNiyHack <niyantavaidya2030@gmail.com>
2026-02-12 08:57:28 +0545

    Secondjs done

---
+++ second.js
@@ -0,0 +1,12 @@
+const sentence = 'Ronaldo is the best football player in the world. He has won many awards and is loved by fans all over the world.';
+
+
+const replaceName = sentence.replace('Ronaldo', 'Messi');
+console.log(replaceName);
+
+
+const upperCaseSentence = sentence.toUpperCase();
+console.log(upperCaseSentence);
+
+const replaceAllThe = sentence.replaceAll('the', 'd');
+console.log(replaceAllThe);
