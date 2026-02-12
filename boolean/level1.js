011344a1cb5e11623d0c5d9dbb1a8a21513a4663
Satish Shrestha <satish020820@gmail.com>
2026-02-12 09:10:10 +0545

    boolean level 1

--- boolean/level1.js
+++ boolean/level1.js
@@ -2,8 +2,12 @@ let a =10
 let b = 20
 
 // Q1. check if a is greater than b
+console.log(a > b)
 
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+const product = 10 * a
+console.log(product >= b)
 
 // Q3. check if b is divisible by a
+console.log(b % a === 0)
