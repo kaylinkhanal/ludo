634b9c3e7dcda0ca8d2ac24b21a5efb937f9c493
Alisa Bogati <alaskabogati@Alishas-MacBook-Air.local>
2026-02-13 01:50:38 +1100

    JsNumbers level2 completed

--- numbers/level2.js
+++ numbers/level2.js
@@ -2,12 +2,14 @@ const number = 0.3445223
 
 
 // Q1. output should be 0.34
+ const output = number.toFixed(2)
 
-
+ console.log(output)
 
 const age = -30
 
 // Q2 age cannot be negative, output should be 30
 
-
+const ageop =  Math.abs(age)
+console.log(ageop)
 
