928ffffdc69ac056624569fdb1518517ae82aa2a
Alisa Bogati <alaskabogati@Alishas-MacBook-Air.local>
2026-02-13 00:48:42 +1100

    numbers level1 complete

--- numbers/level1.js
+++ numbers/level1.js
@@ -1,8 +1,20 @@
 //Q1 create two varaibles a and b, assign them values, and add their sum to c
+let a = 20
+let b = 30
+let c = a + b
+console.log(c)
 
 //Q2. Create a variable called d and assign it the value of a multiplied by b
+let d = a * b
+console.log(d)
 
 //Q3. convert the type of age to number
 const age = '50'
+const agenum = Number(age)
+console.log(agenum)
 
 //Q4 caclualte the remainder of 10 divided by 3 and assign it to a variable called remainder
+let x = 10
+let y = 3
+let remainder = x % y
+console.log(remainder)
--- strings/level1.js
+++ strings/level1.js
@@ -3,9 +3,10 @@ const name = 'Ram Thapa'
 
 // Q1. output should be 'Hello Ram Thapa, welcome to JavaScript!'
 
+console.log('Hello '+ name)
 
 //Q2. Output should be 9
-
+console.log(length)
 
 // Q3. output should be Ra
 
