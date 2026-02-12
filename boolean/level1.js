0327bb767a85ba20c53b282e716a8974bef92765
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-12 09:14:01 +0545

    output of level 1

--- boolean/level1.js
+++ boolean/level1.js
@@ -1,9 +1,13 @@
 let a =10
 let b = 20
 
-// Q1. check if a is greater than b
-
+// Q1. check if a is greater than b 
+console.log(a>b)
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+let result = (10 * a) >= b;
+console.log(result);
 
 // Q3. check if b is divisible by a
+let div = (b%a == 0)
+console.log(div)
