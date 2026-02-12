c0667595cf3f23f8fae8997a7ab446b25d118052
Aalok-sah14 <aaloksah1003@gmail.com>
2026-02-12 08:33:58 +0545

    output of level 1

---
+++ numbers/level4.js
@@ -0,0 +1,16 @@
+/**Q1: The Tax Calculator Function Create a function called calculateTotal that takes a price and a taxRate. 
+  It should return the total price rounded to 2 decimal places. Example: calculateTotal(100, 0.15) should return 115.00.*/
+
+
+
+
+
+/*Q2: The "Even" Filter Create an array of numbers: [1, 2, 3, 4, 5, 6]. 
+  Use a loop or an array method to create a new array that only contains the even numbers.*/
+
+
+
+
+
+/*Q3: Object Mapping Create an object representing a car with properties for brand, model, and year.
+  Write a script that checks if the car is "antique" (older than 25 years from the current year 2026). */
--- strings/level1.js
+++ strings/level1.js
@@ -2,10 +2,13 @@ const name = 'Ram Thapa'
 
 
 // Q1. output should be 'Hello Ram Thapa, welcome to JavaScript!'
-
+console.log('hello ' + name, ',welcome to  JavaScript!')
 
 //Q2. Output should be 9
-
+const charcount = name.length;
+console.log (charcount)
 
 // Q3. output should be Ra
 
+let output = name.toString().slice(0, 2);
+console.log (output)
