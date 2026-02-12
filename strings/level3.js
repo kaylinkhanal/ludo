60c6c59c716c04287883b0f3908f0a261a97833f
DikpalChaudhary <you@example.com>
2026-02-12 09:02:47 +0545

    level3

---
+++ strings/level3.js
@@ -0,0 +1,24 @@
+// 1. Clean & Change
+// Task: Remove the spaces AND make it uppercase in one line.
+const city = "   new york   "
+console.log(city)
+const cityspace= city.trim().toUpperCase()
+console.log(cityspace)
+// Hint: city.trim().toUpperCase()
+
+
+// 2. Verification
+// Task: Check if the email below ends with ".com".
+const email = "test@gmail.com";
+const endeamil= email.endsWith(".com")
+console.log(endeamil)
+// Hint: Use .endsWith(".com")
+
+
+// 3. Finding the Position
+// Task: Find what number (index) the word "Apple" starts at.
+const fruit = "I have an Apple in my bag";
+const fruitindex = fruit.indexOf('Apple')
+console.log(fruitindex)
+// Hint: fruit.indexOf("Apple")
+
