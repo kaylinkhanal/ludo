23203de769efcfef5a88f8a5e0d06f85697eb7b4
gomachand13 <you@example.com>
2026-02-12 08:19:36 +0545

    level4

--- numbers/Level4.js
+++ numbers/Level4.js
@@ -1,3 +1,22 @@
-const name = "john";
+// Q1. Convert seconds into minutes.
 
-// Q4: Name should always start with a capital letter, output should be "Hello, John!"
+let second =180
+
+let minutes = second/60
+console.log(minutes)
+
+// Q2. Swap two numbers without using a third variable.
+let x=2
+let y=3
+x=x+y
+y=x-y
+x=x-y
+console.log(x)
+console.log(y)
+
+// Q3. Round a number using Math.round().
+let num=8.45
+let rounded=Math.round(num)
+console.log(rounded)
+
+
