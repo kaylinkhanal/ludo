670cb1937c90291c1041ce0ff76b4b10282edd49
gomachand13 <you@example.com>
2026-02-12 20:54:39 +0545

    Level2.js

--- strings/level2.js
+++ strings/level2.js
@@ -2,11 +2,16 @@ const sentence = 'Ronaldo is the best football player in the world. He has won m
 
 // 1. replace the first word Ronaldo  with Messi
 // hint : use the replace() method
+const newsentence=sentence.replace('Ronaldo','messi');
+console.log(newsentence);
 
 
 //2. Convert the entire sentence to uppercase letters
-
+const uppercase=sentence.toUpperCase()
+console.log(uppercase)
 
 
 //3. replace all the word the with 'd'
 //expected output is 'Ronaldo is d best football player in d world. He has won many awards and is loved by fans all over d world.' 
+let result=sentence.replace('the','d');
+console.log(result);
