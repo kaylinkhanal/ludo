c8bdbb762d2fe0fcacbc1e71e8e011a3953ea5db
gomachand13 <you@example.com>
2026-02-12 08:09:22 +0545

    level3

--- numbers/Level3.js
+++ numbers/Level3.js
@@ -5,3 +5,27 @@ const score = 85;
 
 const number=score>=50;
 console.log(number)
+
+// Q2. Create three variables a, b, and c.
+// Find their average and store it in a variable called average.
+// Print the result.
+
+let a = 10
+let b = 20
+let c = 30
+
+let average = (a + b + c) / 3
+console.log(average)
+
+// Q3. Create a variable called num.
+// Find the square and cube of the number.
+// Print both results.
+
+let num = 4
+
+let square = num * num
+let cube = num * num * num
+
+console.log(square)
+console.log(cube)
+
