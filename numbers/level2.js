e254d6bd46ff9ee9d943eb46bc424b3e1984c81e
gomachand13 <you@example.com>
2026-02-12 07:44:51 +0545

    level2

--- numbers/level2.js
+++ numbers/level2.js
@@ -2,12 +2,14 @@ const number = 0.3445223
 
 
 // Q1. output should be 0.34
-
+console.log(Math.round(number*100)/100);
 
 
 const age = -30
 
 // Q2 age cannot be negative, output should be 30
+let positivenumber=Math.abs(age)
+console.log(positivenumber)
 
 
 
